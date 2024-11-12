4de46cb2206c6f83e9edc5dc3c34e08e2aacf6c7
Fat1512 <letanphat15122004@gmail.com>
2024-11-13 00:04:59 +0700

    product_detail

---
+++ app/product_detail.py
@@ -0,0 +1,52 @@
+import json
+import numpy as np
+from selenium import webdriver
+from time import sleep
+import random
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.fahasa.com/truong-ca-achilles.html?fhs_campaign=CATEGORY')
+
+#Get images
+images = driver.find_elements(By.CSS_SELECTOR, ".lightgallery .include-in-gallery")
+image_hrefs = [image.get_attribute("href") for image in images]
+
+#Get description
+more = driver.find_element(By.CSS_SELECTOR, ".fhs_btn_new_default.mobile_link")
+sleep(2)
+more.click()
+
+description = driver.find_element(By.CSS_SELECTOR, "#product_tabs_description_contents").text
+
+#Get detail information
+detail_information = {}
+additional_datas = driver.find_elements(By.CSS_SELECTOR, ".data-table.table-additional tbody tr")
+
+i = 0
+for additional_data in additional_datas:
+    key = additional_data.find_element(By.CSS_SELECTOR, ".table-label").text
+    value = additional_data.find_element(By.CSS_SELECTOR, ".data_{index}".format(index=i)).text
+    detail_information[key] = value
+    i = i + 1
+print(detail_information)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/test.py
+++ app/test.py
@@ -3,7 +3,46 @@ import json
 
 data = []
 
-with open('data/vietnamese_categories.csv') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row['id'])
+# with open('data/vietnamese_categories.csv') as f:
+#     reader = csv.DictReader(f)
+#     for row in reader:
+#         print(row['id'])
+
+import requests
+import json
+import pandas as pd
+#Get category based on category_id
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+    'Cookie': 'frontend=7f33e2db865d465d82fd1b2f46ba5c51; frontend_cid=53bSalvZ3G3c0MJX; external_no_cache=1',
+    'Connection': 'keep-alive'
+}
+
+prefix_url = 'https://www.fahasa.com/node_api/flashsale/product'
+
+response = requests.get(prefix_url, headers=headers).json()
+print(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
