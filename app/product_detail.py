cabe66bb63d8f786f15dafa29e4cbeb10c3f7722
Fat1512 <letanphat15122004@gmail.com>
2024-11-13 00:09:05 +0700

    done

--- app/product_detail.py
+++ app/product_detail.py
@@ -7,9 +7,12 @@ from selenium.common.exceptions import NoSuchElementException, ElementNotInterac
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.fahasa.com/truong-ca-achilles.html?fhs_campaign=CATEGORY')
+
+detail_information = {}
 
 #Get images
 images = driver.find_elements(By.CSS_SELECTOR, ".lightgallery .include-in-gallery")
@@ -23,7 +26,6 @@ more.click()
 description = driver.find_element(By.CSS_SELECTOR, "#product_tabs_description_contents").text
 
 #Get detail information
-detail_information = {}
 additional_datas = driver.find_elements(By.CSS_SELECTOR, ".data-table.table-additional tbody tr")
 
 i = 0
@@ -32,7 +34,13 @@ for additional_data in additional_datas:
     value = additional_data.find_element(By.CSS_SELECTOR, ".data_{index}".format(index=i)).text
     detail_information[key] = value
     i = i + 1
-print(detail_information)
+
+detail_information['images'] = image_hrefs
+detail_information['description'] = description
+with open("data/js.json", "w") as f:
+    json.dump(detail_information, f, ensure_ascii=False, indent=4)
+    f.close()
+
 
 
 
