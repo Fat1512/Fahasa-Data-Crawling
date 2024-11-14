ff42b20c64c0991c2a8f0eb993ddb8a6ee62dc3d
Fat1512 <letanphat15122004@gmail.com>
2024-11-14 13:42:25 +0700

    init

--- app/category.py
+++ app/category.py
@@ -1,5 +1,11 @@
+from unicodedata import category
+
 import requests
 import json
+
+from numpy.ma.core import empty
+
+from app.product_detail import get_product_detail
 import pandas as pd
 
 #Get all the categories
@@ -19,13 +25,32 @@ headers = {
     'Connection': 'keep-alive'
 }
 
+#Fetch data
 prefix_url = 'https://www.fahasa.com/fahasa_catalog/product/loadCatalog'
-
 response = requests.get(prefix_url, params=params, headers=headers).json()
-categories = response['children_categories']
 
+#Fetch categories
+categories = response['children_categories']
 df_category = pd.DataFrame(categories)
 df_category.to_csv('vietnamese_categories.csv', index=False)
+
+# for category in categories:
+for i in range(6, 7):
+    category = categories[i]
+    params['category_id'] = category['id']
+    category_info_response = requests.get(prefix_url, params=params, headers=headers).json()
+
+    product_list = category_info_response['product_list']
+    df_category_product = pd.DataFrame(product_list)
+    df_category_product.to_csv("data/category_id_{category_id}.csv".format(category_id=category['id']), index=False)
+    empty = []
+
+    for product in product_list:
+        empty.append(get_product_detail(url = product['product_url'], category_id = category['id'], product_id=product['product_id']))
+    with open("data/product/{category_id}.json".format(category_id=category['id']), "w") as f:
+        json.dump(empty, f, ensure_ascii=False, indent=4)
+        f.close()
+
 
 # f = open("categories.json", "w", encoding="utf-8")
 # json.dump(categories, f, ensure_ascii=False, indent=4)
--- app/product_detail.py
+++ app/product_detail.py
@@ -7,39 +7,51 @@ from selenium.common.exceptions import NoSuchElementException, ElementNotInterac
 from selenium.webdriver.common.by import By
 import pandas as pd
 
+def get_product_detail(url, category_id, product_id):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('{url}?fhs_campaign=CATEGORY'.format(url=url))
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get('https://www.fahasa.com/truong-ca-achilles.html?fhs_campaign=CATEGORY')
+    detail_information = {}
 
-detail_information = {}
+    #Get images
+    try:
+        images = driver.find_elements(By.CSS_SELECTOR, ".lightgallery .include-in-gallery")
+        image_hrefs = [image.get_attribute("href") for image in images]
+        detail_information['images'] = image_hrefs
+    except Exception:
+        print("Cannot click")
 
-#Get images
-images = driver.find_elements(By.CSS_SELECTOR, ".lightgallery .include-in-gallery")
-image_hrefs = [image.get_attribute("href") for image in images]
+    #Get description
+    try:
+        more = driver.find_element(By.CSS_SELECTOR, ".fhs_btn_new_default.mobile_link")
+        sleep(2)
+        more.click()
+    except Exception:
+        print("Cannot click")
 
-#Get description
-more = driver.find_element(By.CSS_SELECTOR, ".fhs_btn_new_default.mobile_link")
-sleep(2)
-more.click()
+    try:
+        description = driver.find_element(By.CSS_SELECTOR, "#product_tabs_description_contents").text
+        detail_information['description'] = description
+    except Exception:
+        print("Cannot find description")
 
-description = driver.find_element(By.CSS_SELECTOR, "#product_tabs_description_contents").text
+    #Get detail information
+    additional_datas = driver.find_elements(By.CSS_SELECTOR, ".data-table.table-additional tbody tr")
 
-#Get detail information
-additional_datas = driver.find_elements(By.CSS_SELECTOR, ".data-table.table-additional tbody tr")
+    i = 0
+    for additional_data in additional_datas:
+        key = additional_data.find_element(By.CSS_SELECTOR, ".table-label").text
+        value = additional_data.find_element(By.CSS_SELECTOR, ".data_{index}".format(index=i)).text
+        detail_information[key] = value
+        i = i + 1
 
-i = 0
-for additional_data in additional_datas:
-    key = additional_data.find_element(By.CSS_SELECTOR, ".table-label").text
-    value = additional_data.find_element(By.CSS_SELECTOR, ".data_{index}".format(index=i)).text
-    detail_information[key] = value
-    i = i + 1
-
-detail_information['images'] = image_hrefs
-detail_information['description'] = description
-with open("data/js.json", "w") as f:
-    json.dump(detail_information, f, ensure_ascii=False, indent=4)
-    f.close()
+    detail_information['product_id'] = product_id
+    detail_information['category_id'] = category_id
+    return detail_information
+    # with open("data/product/{category_id}_{product_id}.json".format(product_id=product_id, category_id=category_id), "w") as f:
+    #     json.dump(detail_information, f, ensure_ascii=False, indent=4)
+    #     f.close()
 
 
 
